42506b38928f3f17850470a3de424a61791fab54
athersk <95748241+athersk@users.noreply.github.com>
2025-06-26 12:09:20 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.127.32.153:9000/',
+  serverUrl: 'http://43.204.232.127:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
